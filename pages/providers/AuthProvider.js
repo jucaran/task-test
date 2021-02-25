ea9c8d438f3be8beaf876b4d796ff977454b5b63
Juanque <juan.castro.arancibia@gmail.com>
2021-02-24 21:19:17 -0300

    fixes not-component build error

--- pages/providers/AuthProvider.js
+++ pages/providers/AuthProvider.js
@@ -1,5 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { auth, googleProvider } from "./firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCa1XBHyTBE3bonpdoWaj59qlrwOc9dVos",
+  authDomain: "task-test-api.firebaseapp.com",
+  projectId: "task-test-api",
+  storageBucket: "task-test-api.appspot.com",
+  messagingSenderId: "724153991861",
+  appId: "1:724153991861:web:9a3c37806555b1e55eb4fc",
+};
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const auth = firebase.auth();
 
 export const AuthContext = React.createContext({
   user: null,
